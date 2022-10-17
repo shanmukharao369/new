b3f1b0314c4f401f2b7d1924702dc172ab36a456
shanmukharao369 <shanmukha.pediredla@gmail.com>
2022-10-17 12:36:50 +0530

    task3

--- dom.js
+++ dom.js
@@ -1,26 +1,15 @@
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-//document.title = 'shannu';//
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-document.all[10].textContent= 'Hello';
-console.log(document.forms);
-console.log(document.links);
-console.log(document.images);
-//console.log(document.getElementById("header-title"));
-const x = document.getElementById('header-title');
-console.log(x);
-const y = document.getElementById('main-header');
-x.textContent = 'hello';
-x.innerText = 'goodbye';
-console.log(x.textContent);
-console.log(x.innerText);
-x.innerHTML = '<h3>hello</h3>';
-y.style.borderBottom = 'solid 3px #000';
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hello';
+items[2].style.backgroundColor = 'green';
+for (var i = 0; i<items.length;i++){
+    items[i].style.backgroundColor='#f0f0f0';
+}
+for (var i = 0; i<items.length;i++){
+    items[i].style.fontWeight= 'bold';
+}
+
+    
 
 
