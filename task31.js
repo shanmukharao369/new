815fa1d65213ac4c210897122a0cbdf7647f13b9
shanmukharao369 <shanmukha.pediredla@gmail.com>
2022-10-17 16:42:53 +0530

    task-3

---
+++ task31.js
@@ -0,0 +1,6 @@
+const y = document.getElementById('main-header');
+y.style.borderBottom = 'solid 3px #000';
+const z = document.getElementsByClassName('title');
+console.log(z[0]);
+z[0].style.fontWeight = 'bold';
+z[0].style.color = 'green';
